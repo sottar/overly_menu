db6bd27112898e91c1270cccaceed1968a77bf4f
sottar <ohrst.18@gmail.com>
2016-02-14 17:13:25 +0900

    add .overly_sentence and modified

--- src/js/jquery.overly.js
+++ src/js/jquery.overly.js
@@ -11,6 +11,7 @@
 
     var elements = this;
     var settings = $.extend({
+      'speed': 300,
       'color' : 'rgba(0, 0, 0, 0.8)',
       'opacity': '0.8',
     }, options);
@@ -19,11 +20,50 @@
     if(options.color && options.color.match(/#/)) {
       toRGB(options.color);
     }
-    $('#overly_menu').css({
+    $(elements).css({
       'display': 'none',
       'background-color': settings.color
     });
+    $(elements).find('p').addClass('overly_sentence');
+    $(elements).find('a').addClass('overly_links');
 
+
+    // btn
+    var btn_block = $('#overly_open').parent();
+    btn_block.addClass('overly_btn_block')
+    $('#overly_open').addClass('on');
+    $('#overly_open, #overly_close').on('click', function() {
+      $('#overly_open').toggleClass('on');
+      $('#overly_close').toggleClass('on');
+    });
+
+    // menu design
+    var windowHeight = $(window).height();
+    var menuHeight = $(elements).height();
+    var menuMargin = (windowHeight - menuHeight) / 2;
+    $(elements).css({
+      'height': windowHeight
+    });
+    $('#overly_menu ul').css({
+      'padding-top': menuMargin
+    });
+    $('')
+
+    // slide
+    $('#overly_open').on('click', function() {
+      $(elements).animate({
+        height: 'show',
+        opacity: 'show'
+      }, settings.speed);
+    });
+    $('#overly_close').on('click', function() {
+      $(elements).animate({
+        height: 'hide',
+        opacity: 'hide'
+      }, settings.speed);
+    });
+
+    // convert from #xxxxxx to rgba()
     function toRGB(color16) {
       var tmpColor16 = color16.substr(1)
       var a16 = new Array();
@@ -43,38 +83,5 @@
       settings.color = color10;
     }
 
-    // btn
-    var btn_block = $('#overly_open').parent();
-    btn_block.addClass('overly_btn_block')
-    $('#overly_open').addClass('on');
-    $('#overly_open, #overly_close').on('click', function() {
-      $('#overly_open').toggleClass('on');
-      $('#overly_close').toggleClass('on');
-    });
-
-    // design
-    var windowHeight = $(window).height();
-    var menuHeight = $(elements).height();
-    var menuMargin = (windowHeight - menuHeight) / 2;
-    $('#overly_menu').css({
-      'height': windowHeight
-    });
-    $('#overly_menu ul').css({
-      'padding-top': menuMargin
-    });
-
-    // slide menu
-    $('#overly_open').on('click', function() {
-      $('#overly_menu').animate({
-        height: 'show',
-        opacity: 'show'
-      }, settings.speed);
-    });
-    $('#overly_close').on('click', function() {
-      $('#overly_menu').animate({
-        height: 'hide',
-        opacity: 'hide'
-      }, settings.speed);
-    });
   };
 })(jQuery);
