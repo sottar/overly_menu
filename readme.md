c6b5920b631d578dc38d01174cdd37c27504fb1c
sottar <ohrst.18@gmail.com>
2016-02-27 10:13:49 +0900

    add demo, update README, add minify file

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Overly_menu
+Overly_menu is a jQuery plugin for the easiest way for make smart creating global menus 
+
+## Usage
+
+[.zip Download (Mac)](http://www.sottar.net/dist/overly_menu_master.zip)  
+~~[.zip Download (Win)]()~~  
+
+
+    <script>
+      $(function() {
+        $('#overly_menu').overly_menu();
+      });
+    </script>
+
+### Options  
+ Options: default  
+    'speed': 300,  
+    'color' : 'rgba(0, 0, 0, 0.8)',  
+    'opacity': 0.8,  
+    'style': 'list'  
+
+
+## DEMO
+
+<img src="http://www.sottar.net/demo/overly_menu/demo_simple.gif" width="200" style="border:1px solid #333;">  
+<img src="http://www.sottar.net/demo/overly_menu/demo_flex.gif" width="300" border="1px solid #333">
+
+## LISENCE
+Code copyright 2016 Sota Ohara. Code released under the MIT license.  
--- src/js/jquery.overly.js
+++ src/js/jquery.overly.js
@@ -13,9 +13,8 @@
     var settings = $.extend({
       'speed': 300,
       'color' : 'rgba(0, 0, 0, 0.8)',
-      'opacity': '0.8',
-      'style': 'list',
-      'hover': 'font-size'
+      'opacity': 0.8,
+      'style': 'list'
     }, options);
 
     //init
@@ -28,32 +27,7 @@
     });
     $(elements).find('p').parent('li').addClass('overly_sentence');
     $(elements).find('a').parent('li').addClass('overly_links');
-    $('.overly_links').find('a').hover(function() {
-      if(settings.hover === 'opacity') {
-        $(this).css({'opacity': 0.8})
-      } else if (settings.hover === 'underline') {
-        $(this).css({'text-decoration': 'underline'})
-      } else if (settings.hover === 'font-weight') {
-        $(this).css({'font-weight': 'normal'})
-      } else if (settings.hover === 'letter-spacing') {
-        $(this).css({'letter-spacing': '2px'})
-      } else {
-        $(this).css({'font-size': '30px'})
-      }
-    },
-    function() {
-      if(settings.hover === 'opacity') {
-        $(this).css({'opacity': 1})
-      } else if (settings.hover === 'underline') {
-        $(this).css({'text-decoration': 'none'})
-      } else if (settings.hover === 'font-weight') {
-        $(this).css({'font-weight': '200'})
-      } else if (settings.hover === 'letter-spacing') {
-        $(this).css({'letter-spacing': '0px'})
-      } else {
-        $(this).css({'font-size': '24px'})
-      }
-    });
+
 
     // btn
     var btn_block = $('#overly_open').parent();
@@ -94,24 +68,21 @@
       }, settings.speed);
     });
 
-    // convert from #xxx to rgba()
+    // convert from #xxxxxx to rgba()
     function toRGB(color16) {
       var tmpColor16 = color16.substr(1)
       var array16 = new Array();
       var array10 = new Array();
-      console.log(tmpColor16.length);
       if(tmpColor16.length == 6) {
         for(var i=1; i<=3; i++) {
           array16[i-1] = tmpColor16.substr(2 * i - 2, 2);
         }
       } else if (tmpColor16.length == 3) {
         array16 = tmpColor16.split("")
-        console.log(array16);
       }
       for(var t=0; t<=2; t++) {
         array10[t] = parseInt(array16[t], 16);
       }
-      console.log(array10);
       if(settings.opacity) {
         var a = settings.opacity;
       } else {
